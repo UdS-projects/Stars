9e2fc6771020ab43094c1db1501849eae77b8073
Fatih Demirkan <s8fademi@stud.uni-saarland.de>
2019-05-20 14:23:41 +0200

    fertig2

--- main.c
+++ main.c
@@ -26,12 +26,16 @@ int main(int argc, char *argv[])
 	// TODO: Read in the stars from the file with name argv[2] 
 	// save them in an array in the order they are read in and draw them to the image.
     FILE* fp = fopen(argv[2], "r");
+    char c ; 
     
-    double x,y,z,magnitude;
-    int draper, harvard;
+    //double x,y,z,magnitude;
+    //int draper, harvard;
     int fileSize = 0;
-	while(fscanf(fp, "%lf %lf %lf %i %lf %i", &x, &y, &z, &draper, &magnitude, &harvard) == 1)
+// 	while(fscanf(fp, "%lf %lf %lf %i %lf %i", &x, &y, &z, &draper, &magnitude, &harvard) == 1)
+    while (c != EOF )
     {
+        c = fgetc(fp);
+        if (c == '\n');
         fileSize++;
     }
     rewind(fp);
